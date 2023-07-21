3a476ac16b73333874ebf81b6e38cf5cf9e0dc79
divy-a <121391127+divy-a@users.noreply.github.com>
2023-07-21 14:31:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 movie_name = str(os.getenv('MOVIE_NAME', default=''))
-
-threading.Thread(target=utils.keep_seaching,
-                    args=(movie_name, 600,)).start()
+route_id = str(os.getenv('RID', default=''))
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'INDEX'
+
+@app.route(f'/{route_id}')
+def index():
+    utils.ms(movie_name)
+    return 'ROUTE', 200
 
 if __name__ == '__main__':
     app.run()
